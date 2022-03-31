f21b7d38eb65f532c922dce850d0f67c96b95c30
Felipe Vergara <favazcl@gmail.com>
2022-03-31 14:49:11 -0300

    add weather api package

---
+++ packages/weather_api/lib/src/weather_api.dart
@@ -0,0 +1,7 @@
+/// {@template weather_api}
+/// The interface and models for an API providing access to weather.
+/// {@endtemplate}
+class WeatherApi {
+  /// {@macro weather_api}
+  const WeatherApi();
+}
