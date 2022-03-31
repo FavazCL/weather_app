009f0bf0dd165672652fa3dfaa283aea1fd35f53
Felipe Vergara <favazcl@gmail.com>
2022-03-31 14:50:25 -0300

    add repository package

---
+++ packages/weather_repository/lib/src/weather_repository.dart
@@ -0,0 +1,7 @@
+/// {@template weather_repository}
+/// A repository that handles weather related requests.
+/// {@endtemplate}
+class WeatherRepository {
+  /// {@macro weather_repository}
+  const WeatherRepository();
+}
