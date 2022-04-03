7b2d27438baeaf371e7ea7d86528a45c87f556f6
Felipe Vergara <favazcl@gmail.com>
2022-04-03 18:57:39 -0400

    add documentation to utils

--- packages/utils/lib/src/format_date.dart
+++ packages/utils/lib/src/format_date.dart
@@ -1,11 +1,14 @@
 import 'package:intl/intl.dart';
 
+/// Class FormatDate
 class FormatDate {
+  /// Static function that format a given date and return a [String]
   static String getDayOfWeek(DateTime dateTime) {
-    return DateFormat('EEEE, d').format(dateTime).toString();
+    return DateFormat('EEEE, d').format(dateTime);
   }
 
+  /// Static function that format a given date and return a [String]
   static String getJustDayOfWeek(DateTime dateTime) {
-    return DateFormat('EEEE').format(dateTime).toString();
+    return DateFormat('EEEE').format(dateTime);
   }
 }
--- packages/utils/lib/src/package_name.dart
+++ packages/utils/lib/src/package_name.dart
@@ -1,3 +1,5 @@
+/// PackageName class to handler the  names of the modules
 class PackageName {
+  /// Static variable to handler ui_kit module
   static const uiKit = 'ui_kit';
 }
